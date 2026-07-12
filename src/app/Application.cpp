69821c834b2ac9dc823f3d2c9ffe9cf8b8eeaee2
NicoAICP <24915669+NicoAICP@users.noreply.github.com>
2026-07-12 22:17:09 +0200

    Implement File Appending

--- src/app/Application.cpp
+++ src/app/Application.cpp
@@ -25,46 +25,9 @@ void Application::Initialize()
     m_led.Init();
     m_buttons.Init();
     m_display.Init();
-    if (m_storage.Init())
+    if (!m_storage.Init())
     {
-        m_display.DrawText(5,5,"MOUNT OK");
-    }
-    else
-    {
-        m_display.DrawText(5,5,"MOUNT FAIL");
-    }
-    
-    std::vector<std::uint8_t> data =
-    {
-    'H',
-    'e',
-    'l',
-    'l',
-    'o',
-    ' ',
-    'A',
-    'T',
-    'L',
-    'A',
-    'S'
-    };
-
-    if (m_storage.WriteFile("/hello.txt", data))
-    {
-        m_display.DrawText(5,15,"WRITE OK");
-    }
-    else
-    {
-        m_display.DrawText(5,15,"WRITE FAIL");
-    }
-    
-    std::vector<std::uint8_t> file;
-
-    if (m_storage.ReadFile("/hello.txt", file))
-    {
-        file.push_back('\0');
-
-        m_display.DrawText(5,25,reinterpret_cast<char*>(file.data()));
+        m_led.SetPattern(LedPattern::Error);
     }
 
 }
--- src/storage/Storage.cpp
+++ src/storage/Storage.cpp
@@ -120,10 +120,33 @@ bool Storage::WriteFile(
 }
 
 bool Storage::AppendFile(
-    const std::string&,
-    const std::vector<std::uint8_t>&)
+    const std::string& path,
+    const std::vector<std::uint8_t>& data)
 {
-    return false;
+    FIL file;
+
+    if (f_open(
+            &file,
+            path.c_str(),
+            FA_OPEN_APPEND | FA_WRITE) != FR_OK)
+    {
+        return false;
+    }
+
+    UINT bytesWritten = 0;
+
+    const FRESULT result =
+        f_write(
+            &file,
+            data.data(),
+            static_cast<UINT>(data.size()),
+            &bytesWritten);
+
+    f_close(&file);
+
+    return
+        result == FR_OK &&
+        bytesWritten == data.size();
 }
 
 std::vector<DirectoryEntry>
